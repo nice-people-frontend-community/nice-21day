a22f346ac10a0c29349f387d62ec52e68d5dc8e7
Anonymity94 <1176989591@qq.com>
2022-07-02 20:23:44 +0800

    fix: base path

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,6 @@
 export default {
   npmClient: "pnpm",
   title: "学习训练营证书生成器",
+  base: process.env.NODE_ENV === "production" ? "/nice-21day" : "/",
   extraBabelPlugins: [["import", { libraryName: "antd", style: true }]],
 };
