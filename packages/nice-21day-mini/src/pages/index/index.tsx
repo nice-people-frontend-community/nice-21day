2cc5b4e5776184c51e6e43d08a8050dee5574316
Anonymity94 <1176989591@qq.com>
2022-07-23 22:38:56 +0800

    fix(mini): dev

--- packages/nice-21day-mini/src/pages/index/index.tsx
+++ packages/nice-21day-mini/src/pages/index/index.tsx
@@ -1,18 +1,18 @@
-import { View } from "@tarojs/components";
-import { AtButton } from "taro-ui";
-
-import Taro from "@tarojs/taro";
-import "taro-ui/dist/style/components/button.scss"; // 按需引入
-import "./index.less";
+import { View } from '@tarojs/components';
+import { AtButton } from 'taro-ui';
+import React from 'react';
+import Taro from '@tarojs/taro';
+import 'taro-ui/dist/style/components/button.scss'; // 按需引入
+import './index.less';
 
 function IndexPage() {
   const login = () => {
     Taro.getUserProfile({
-      desc: "用于完善会员资料", // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: res => {
+      desc: '用于完善会员资料', // 声明Ø获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
         // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
         console.log(res);
-      }
+      },
     });
   };
 
